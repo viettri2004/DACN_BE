a4507a556639e64656f2726e82186dabee2147a9
viettri2004 <tonviettri2004@gmail.com>
2025-10-10 01:59:00 +0700

    feat(course): add image URL and price fields to course entity

--- src/Services/CourseService/Domain/Entities/Course.cs
+++ src/Services/CourseService/Domain/Entities/Course.cs
@@ -9,7 +9,9 @@ namespace Entities
     public class Course
     {
         public string Id { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
         public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
         public string Description { get; set; } = null!;
         public DateTime CreateTime { get; set; } = DateTime.UtcNow;
         public string InstructorId { get; set; } = null!;
